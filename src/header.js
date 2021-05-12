336637aa30647d393aa3962675c63f740ddacbd4
Robert Erickson <bobericksonjr@gmail.com>
2021-05-12 14:37:02 -0400

    adds header JavaScript rendering

---
+++ src/header.js
@@ -0,0 +1,66 @@
+const getHeader = (function() {
+    // DOM
+    const content = document.querySelector('#content');
+    
+    // DOM Creation
+    const header = makeHeader();
+
+    // Dom Assembly
+
+    content.appendChild(header);
+
+    function makeHeader() {
+        const headerElement = document.createElement('header');
+        headerElement.classList.add('menu', 'span-12')
+
+        const h1Element = document.createElement('h1');
+        h1Element.classList.add('menu__header');
+
+        const h1LinkElement = document.createElement('a');
+        h1LinkElement.classList.add('link', 'header__link');
+        h1LinkElement.setAttribute('href', '');
+        h1LinkElement.innerText = 'Dandelion Greens';
+
+        h1Element.appendChild(h1LinkElement);
+        headerElement.appendChild(h1Element);
+
+        const nav = makeNav();
+
+        headerElement.appendChild(nav);
+
+        return headerElement;
+    }
+
+    function makeNav() {
+        const navElement = document.createElement('nav');
+        navElement.classList.add("nav");
+
+        const ulElement = document.createElement('ul');
+        ulElement.classList.add('nav__list');
+
+        const linkNames = ['About', 'Reservations', 'Contact'];
+        linkNames.forEach(linkName => makeLinks(linkName, ulElement));
+
+        navElement.appendChild(ulElement);
+
+        return navElement;
+    }
+
+    function makeLinks(link, ulElement){
+
+        const liElement = document.createElement('li');
+        liElement.classList.add("nav__item");
+
+        const linkElement = document.createElement('a');
+        linkElement.classList.add('link', 'nav__link', link.toLowerCase());
+        linkElement.setAttribute('href', '');
+        linkElement.innerText = link;
+
+        liElement.appendChild(linkElement);
+
+        ulElement.appendChild(liElement);
+    }
+    
+});
+
+export default getHeader
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("working");
+import getHeader from './header';
+import getHomePage from './homePage'
+
+const header = (function() {
+    
+    getHeader();
+    getHomePage();
+
+    // DOM
+    const homeLink = document.querySelector('.header__link');
+    const aboutLink = document.querySelector('.about');
+    const reservationsLink = document.querySelector('.reservations');
+    const contactLink = document.querySelector('.contact');
+
+    // Event Handlers
+
+})();
